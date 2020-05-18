e8e4d9e9346f76b1158993e66b405342f27951fe
egozak31 <59114903+egozak31@users.noreply.github.com>
2020-05-18 14:21:08 +0300

    Create lab_1_code.py

---
+++ lab_1_code.py
@@ -0,0 +1,58 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+# имопрт библиотек
+import pandas as pd
+import math
+
+# загружаем данные
+data = pd.read_csv('data.csv')
+display(data)
+
+abon = 915642913
+
+# тарифы
+k = 1
+sms_5 = 0
+sms5_10 = 1
+sms10_ = 2
+
+incoming_stat = data.query('msisdn_dest == @abon')
+outcoming_stat = data.query('msisdn_origin == @abon')
+
+print("Входящие звонки выбранного абонента:")
+display(incoming_stat)
+print()
+print("Исходящие звонки и смс выбранного абонента:")
+display(outcoming_stat)
+
+# считаем тариф по исходящим звонкам
+outcoming_calls_bill = math.ceil(outcoming_stat['call_duration']) * k
+# считаем тариф по входящим звонкам
+incoming_calls_bill = math.ceil(incoming_stat['call_duration']) * k
+# считаем общий тариф по звонкам
+total_calls_bill = outcoming_calls_bill + incoming_calls_bill
+
+def sms_biller(sms_number):
+  try:
+    if sms_number < 6:
+      return sms_number * sms_5
+    elif sms_number < 11:
+      return (sms_number - 5) * sms5_10
+    elif sms_number >= 11:
+      return 5 + (sms_number - 10) * sms10_
+  except:
+    print('incorrect input')
+
+
+total_sms_bill = sms_biller(int(outcoming_stat['sms_number']))
+
+total_bill = total_calls_bill + total_sms_bill
+
+print("За выбранный период у абонента с номером {} списано {} руб. 00 коп.".format(abon, total_bill))
+
