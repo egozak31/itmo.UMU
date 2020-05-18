a86e35b3b9f4615901e68268cc071d86416fc4fc
egozak31 <59114903+egozak31@users.noreply.github.com>
2020-05-18 14:22:17 +0300

    Update lab_1_code.py

--- lab_1_code.py
+++ lab_1_code.py
@@ -1,11 +1,3 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-
 # имопрт библиотек
 import pandas as pd
 import math
